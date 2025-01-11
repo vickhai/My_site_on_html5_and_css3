ddcd9773144e38d518f31e6a82ff92a23fd06f23
Khaidukova.vic <khaidukova.vic@gmail.com>
2025-01-11 21:05:19 +0700

    project start

---
+++ js.js
@@ -0,0 +1,6 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('.button-link');
+
+button.addEventListener('click', function() {
+	alert('Деньги это главное')
+})
